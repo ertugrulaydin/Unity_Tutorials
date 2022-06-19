3a99ae6feaa360f42978d7e42d3677c1fec6aa21
Ertuğrul Aydın <ertugrulaydin16@gmail.com>
2022-06-19 20:41:33 +0300

    Mover Wall

--- Rocket Game/Assets/Scripts/Concretes/Controllers/MoverWallController.cs
+++ Rocket Game/Assets/Scripts/Concretes/Controllers/MoverWallController.cs
@@ -9,16 +9,27 @@ public class MoverWallController : WallController
     [SerializeField] Vector3 _direction;
     [Range(0f, 1f)]
     [SerializeField] float _factor;
+    [SerializeField] float _speed = 1f;
 
     Vector3 _startPosition;
+    const float FULL_CIRCLE = Mathf.PI * 2f;  //2piR
 
     private void Awake()
     {
         _startPosition = transform.position;
     }
 
+    private void Start()
+    {
+        transform.position = _startPosition;
+    }
+
     private void Update()
     {
+        float cycle = Time.time / _speed;
+        float simWave = Mathf.Sin(cycle * FULL_CIRCLE);
+
+        _factor = Mathf.Abs(simWave);
         Vector3 offset = _direction * _factor;
         transform.position = offset + _startPosition;
     }
